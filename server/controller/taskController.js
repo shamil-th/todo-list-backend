3803b2eee089bbebda74c03a4f5974aae0c6219c
shamil-th <shamil.stackup@gmail.com>
2024-02-23 18:40:52 +0530

    edit

--- server/controller/taskController.js
+++ server/controller/taskController.js
@@ -1,53 +1,75 @@
-const TASK_LIST = require('../model/todoModel');
+const TASK_LIST = require("../model/todoModel");
 
 // create a task
 exports.create = async (req, res) => {
-    try {
-        const { task, priority, dueDate, description, completed } = req.body;
+  try {
+    const { task, priority, dueDate, description, completed } = req.body;
 
-        console.log(req.body)
+    console.log(req.body);
 
-        const newTaskList = new TASK_LIST({
-            task,
-            priority,
-            dueDate,
-            description,
-            completed
-        });
+    const newTaskList = new TASK_LIST({
+      task,
+      priority,
+      dueDate,
+      description,
+      completed,
+    });
 
-        await newTaskList.save();
+    await newTaskList.save();
 
-        res.status(201).json(newTaskList);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Server Error" });
-    }
+    res.status(201).json(newTaskList);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 // retrive all data
 exports.find = async (req, res) => {
-    try {
-        const tasks = await TASK_LIST.find();
-        res.status(200).json(tasks);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error occurred while retrieving data" });
-    }
+  try {
+    const tasks = await TASK_LIST.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error occurred while retrieving data" });
+  }
 };
-
 
 // delte a task
 exports.remove = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await TASK_LIST.findByIdAndDelete(id);
+  try {
+    const id = req.params.id;
+    const data = await TASK_LIST.findByIdAndDelete(id);
 
-        if (!data) {
-            res.status(404).send({ message: `Cannot delete task with id ${id}` });
-        } else {
-            res.send({ message: "Task deleted successfully" });
-        }
-    } catch (error) {
-        res.status(500).send({ message: `Could not delete task: ${error}` });
+    if (!data) {
+      res.status(404).send({ message: `Cannot delete task with id ${id}` });
+    } else {
+      res.send({ message: "Task deleted successfully" });
     }
+  } catch (error) {
+    res.status(500).send({ message: `Could not delete task: ${error}` });
+  }
+};
+
+exports.update = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const tasks = await TASK_LIST.findById(id);
+    if (!tasks) {
+      return res.status(400).send({ message: `Error while updating task` });
+    }
+
+    const updatedData = {
+      ...req.body,
+    };
+
+    const updated = await TASK_LIST.findByIdAndUpdate(id, updatedData, {
+      new: true,
+    });
+    res.status(200).json(updated);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
 };
--- server/router/routes.js
+++ server/router/routes.js
@@ -5,5 +5,6 @@ const taskController = require('../controller/taskController')
 route.post('/',taskController.create);
 route.get('/',taskController.find);
 route.delete('/:id',taskController.remove);
+route.put('/:id',taskController.update);
 
 module.exports = route
